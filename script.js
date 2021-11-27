f479ef37db6a84d066ba631d7b0f886a3d95e9a9
Linh <linhhk15@gmail.com>
2021-11-27 00:31:23 -0800

    Display player and computer choices on the stage

--- script.js
+++ script.js
@@ -1,29 +1,91 @@
-// computer randomly chooses 1 of 3 options
+// function playAGame() {
+//   computerPlay()
+//   playerPlay()
+//   displayComputer()
+//   displayPlayer()
+//   displayResult()
+//   displayScore()
+// }
+//
+//
+
+
+// // computer randomly chooses 1 of 3 options
 function computerPlay() {
     let randomChoice = Math.floor(Math.random() * 3);
-    let comp;
+    let compChoice;
+    let compStage = document.getElementById("comp-stage");
+    compStage.classList.add("fas");
     if (randomChoice == 0) {
-      comp = 'rock';
+      compStage.classList.add("fa-hand-rock");
+      compChoice = "rock";
     } else if (randomChoice == 1) {
-      comp = 'paper';
+      compStage.classList.add("fa-hand-paper");
+      compChoice = "paper";
     } else {
-      comp = 'scissors';
+      compStage.classList.add("fa-hand-scissors");
+      compChoice = "scissors";
     }
-    return comp;
+    return compChoice;
 }
+
+// computerPlay();
+
 
 // user chooses 1 of 3 options
 function playerPlay() {
-    let userInput = window.prompt('Which do you choose? Rock, Paper, Scissors?');
-    let plr = userInput.toLowerCase();
-    return plr;
+    let playerStage = document.getElementById("player-stage");
+    playerStage.classList.add("fas");
+
+    document.getElementById("player-rock").addEventListener("click", () => {
+      playerStage.classList.add("fa-hand-rock");
+      computerPlay();
+      return "rock";
+    });
+
+    document.getElementById("player-paper").addEventListener("click", () => {
+      playerStage.classList.add("fa-hand-paper");
+      let playerChoice = "paper";
+      computerPlay();
+    });
+
+    document.getElementById("player-scissors").addEventListener("click", () => {
+      playerStage.classList.add("fa-hand-scissors");
+      let playerChoice = "scissors";
+      computerPlay();
+    });
+    //
+    // console.log(items);
+    // let choice;
+    //
+    // for (let i = 0; i < items.length; i++) {
+    //   items[i].addEventListener("click", function() {
+    //     let item = items[i];
+    //     if (item.id == "player-rock") {
+    //       choice = "rock";
+    //     } else if (item.id == "player-paper") {
+    //       choice = "paper";
+    //     } else if (item.id == "player-scissors") {
+    //       choice = "scissors";
+    //     }
+    //   });
+    // }
+
 }
+
+// playerPlay();
+
+
+
+
+
+
 
 // play 1 round, message the result of this round at the end
 function playRound(computerSelection, playerSelection) {
   playerSelection = playerPlay();
   computerSelection = computerPlay();
-  console.log("Computer chooses " + computerSelection);
+
   let result;
   if (computerSelection == playerSelection) {
     console.log("It's a tie!")
@@ -39,30 +101,3 @@ function playRound(computerSelection, playerSelection) {
   }
   return result;
 }
-
-// play 5 rounds, keep counts of result in each round, get final result at the end
-
-function game() {
-  let computer = 0;
-  let player = 0;
-  let draw = 0;
-  for (let i = 1; i <= 5; i++) {
-    let resultRound = playRound();
-    if (resultRound == 'draw') {
-      draw++;
-    } else if (resultRound == 'computer') {
-      computer++;
-    } else {
-      player++;
-    }
-  }
-  if (draw > computer && draw > player) {
-    console.log("It's a tie!");
-  } else if (computer > player) {
-    console.log("Computer wins!");
-  } else {
-    console.log("You win!");
-  }
-}
-
-game();
